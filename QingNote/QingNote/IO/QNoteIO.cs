222aa912c31c7ee745e36940b7c7d54d83c76f86
ZengXiao <zuoanqh@live.com>
2014-08-26 19:20:50 -0400

    指令类单立文件了。加入了IO工具类。

---
+++ QingNote/QingNote/IO/QNoteIO.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using cn.zuoanqh.open.zut;
+
+namespace cn.zuoanqh.open.QingNote.IO
+{
+  class QNoteIO
+  {
+    public static IEnumerable<string> FileKeywordsLang = fkwLang;
+    public static IEnumerable<string> SettingsLang = sLang;
+
+    private static Lang fkwLang = new Lang(ResLang.FileKeywords);
+    private static Lang sLang = new Lang(ResLang.Settings);
+
+    public static string getFileLang(string fileName)
+    {
+      if (!fileName.EndsWith(Resources.FilePostfix)) return null;
+      string s = zusp.Left(fileName, fileName.Length - Resources.FilePostfix.Length);
+      if (s.LastIndexOf(".") < 0) return null;
+      s = zusp.ChopTail(s, ".").Second;
+      return s;
+    }
+    public static Boolean isFileLangValid(string fileLang)
+    {
+      return fkwLang.langs.Contains(fileLang);
+    }
+
+
+
+    private class Lang : IEnumerable<string>
+    {
+      public List<string> langs;
+      public Lang(string s)
+      { langs = s.Split(' ').ToList(); }
+
+      public IEnumerator<string> GetEnumerator()
+      {
+        foreach (string s in langs) yield return s;
+      }
+
+      IEnumerator IEnumerable.GetEnumerator()
+      {
+        return GetEnumerator();
+      }
+    }
+  }
+}
---
+++ QingNote/QingNote/Instruction.cs
@@ -0,0 +1,12 @@
+﻿namespace cn.zuoanqh.open.QingNote
+{
+  /// <summary>
+  /// Continue: Do the default behaviour
+  /// Abort_Return_Null: exactly as it says
+  /// Abort_Return_Current: if there's some process, return the process, null or empty otherwise
+  /// Skip: Skip the problem.
+  /// Restart: Start over with same parameters.
+  /// Choose: Choose the one specified by overseer.
+  /// </summary>
+  public enum Instruction { CONTINUE, ABORT_RETURN_NULL, ABORT_RETURN_CURRENT, SKIP, RESTART, CHOOSE }
+}
