3b58b31c8c58eb5560ec58b1347fa093e1e0cbda
ZengXiao <zuoanqh@live.com>
2014-09-15 22:14:11 -0400

    新建卡盒管理窗口。

---
+++ QingNote/QingNote/DialogManageBoxes.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace cn.zuoanqh.open.QingNote
+{
+  partial class DialogManageBoxes
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.listBox1 = new System.Windows.Forms.ListBox();
+      this.lblBoxName = new System.Windows.Forms.Label();
+      this.lblDateCreated = new System.Windows.Forms.Label();
+      this.btnEditBoxName = new System.Windows.Forms.Button();
+      this.lblCreator = new System.Windows.Forms.Label();
+      this.lblBoxIndexing = new System.Windows.Forms.Label();
+      this.listBox2 = new System.Windows.Forms.ListBox();
+      this.lblListCaption = new System.Windows.Forms.Label();
+      this.btnSaveBoxInfo = new System.Windows.Forms.Button();
+      this.buttonOpenBox = new System.Windows.Forms.Button();
+      this.btnAddIndexItem = new System.Windows.Forms.Button();
+      this.btnEditDateCreated = new System.Windows.Forms.Button();
+      this.btnEditCreator = new System.Windows.Forms.Button();
+      this.btnChooseIndexing = new System.Windows.Forms.Button();
+      this.lblDescriptionText = new System.Windows.Forms.Label();
+      this.btnRemoveIndexItem = new System.Windows.Forms.Button();
+      this.lblDescriptionCaption = new System.Windows.Forms.Label();
+      this.btnEditDescription = new System.Windows.Forms.Button();
+      this.btnDeleteBox = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // listBox1
+      // 
+      this.listBox1.FormattingEnabled = true;
+      this.listBox1.ItemHeight = 20;
+      this.listBox1.Location = new System.Drawing.Point(11, 16);
+      this.listBox1.Margin = new System.Windows.Forms.Padding(4);
+      this.listBox1.Name = "listBox1";
+      this.listBox1.Size = new System.Drawing.Size(220, 424);
+      this.listBox1.TabIndex = 0;
+      // 
+      // lblBoxName
+      // 
+      this.lblBoxName.Location = new System.Drawing.Point(454, 10);
+      this.lblBoxName.Name = "lblBoxName";
+      this.lblBoxName.Size = new System.Drawing.Size(202, 30);
+      this.lblBoxName.TabIndex = 1;
+      this.lblBoxName.Text = "\r\n";
+      this.lblBoxName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+      // 
+      // lblDateCreated
+      // 
+      this.lblDateCreated.Location = new System.Drawing.Point(454, 47);
+      this.lblDateCreated.Name = "lblDateCreated";
+      this.lblDateCreated.Size = new System.Drawing.Size(202, 32);
+      this.lblDateCreated.TabIndex = 2;
+      this.lblDateCreated.Text = "\r\n";
+      this.lblDateCreated.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+      // 
+      // btnEditBoxName
+      // 
+      this.btnEditBoxName.Location = new System.Drawing.Point(662, 9);
+      this.btnEditBoxName.Name = "btnEditBoxName";
+      this.btnEditBoxName.Size = new System.Drawing.Size(88, 32);
+      this.btnEditBoxName.TabIndex = 3;
+      this.btnEditBoxName.UseVisualStyleBackColor = true;
+      // 
+      // lblCreator
+      // 
+      this.lblCreator.Location = new System.Drawing.Point(454, 85);
+      this.lblCreator.Name = "lblCreator";
+      this.lblCreator.Size = new System.Drawing.Size(202, 32);
+      this.lblCreator.TabIndex = 4;
+      this.lblCreator.Text = "\r\n";
+      this.lblCreator.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+      // 
+      // lblBoxIndexing
+      // 
+      this.lblBoxIndexing.Location = new System.Drawing.Point(454, 123);
+      this.lblBoxIndexing.Name = "lblBoxIndexing";
+      this.lblBoxIndexing.Size = new System.Drawing.Size(202, 32);
+      this.lblBoxIndexing.TabIndex = 5;
+      this.lblBoxIndexing.Text = "\r\n";
+      this.lblBoxIndexing.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+      // 
+      // listBox2
+      // 
+      this.listBox2.FormattingEnabled = true;
+      this.listBox2.ItemHeight = 20;
+      this.listBox2.Location = new System.Drawing.Point(242, 51);
+      this.listBox2.Name = "listBox2";
+      this.listBox2.Size = new System.Drawing.Size(200, 344);
+      this.listBox2.TabIndex = 6;
+      // 
+      // lblListCaption
+      // 
+      this.lblListCaption.Location = new System.Drawing.Point(238, 16);
+      this.lblListCaption.Name = "lblListCaption";
+      this.lblListCaption.Size = new System.Drawing.Size(204, 32);
+      this.lblListCaption.TabIndex = 7;
+      this.lblListCaption.Text = "\r\n";
+      this.lblListCaption.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+      // 
+      // btnSaveBoxInfo
+      // 
+      this.btnSaveBoxInfo.Location = new System.Drawing.Point(539, 402);
+      this.btnSaveBoxInfo.Name = "btnSaveBoxInfo";
+      this.btnSaveBoxInfo.Size = new System.Drawing.Size(97, 38);
+      this.btnSaveBoxInfo.TabIndex = 8;
+      this.btnSaveBoxInfo.UseVisualStyleBackColor = true;
+      // 
+      // buttonOpenBox
+      // 
+      this.buttonOpenBox.Location = new System.Drawing.Point(645, 402);
+      this.buttonOpenBox.Name = "buttonOpenBox";
+      this.buttonOpenBox.Size = new System.Drawing.Size(105, 38);
+      this.buttonOpenBox.TabIndex = 9;
+      this.buttonOpenBox.UseVisualStyleBackColor = true;
+      // 
+      // btnAddIndexItem
+      // 
+      this.btnAddIndexItem.Location = new System.Drawing.Point(242, 402);
+      this.btnAddIndexItem.Name = "btnAddIndexItem";
+      this.btnAddIndexItem.Size = new System.Drawing.Size(96, 38);
+      this.btnAddIndexItem.TabIndex = 10;
+      this.btnAddIndexItem.UseVisualStyleBackColor = true;
+      // 
+      // btnEditDateCreated
+      // 
+      this.btnEditDateCreated.Location = new System.Drawing.Point(662, 47);
+      this.btnEditDateCreated.Name = "btnEditDateCreated";
+      this.btnEditDateCreated.Size = new System.Drawing.Size(88, 32);
+      this.btnEditDateCreated.TabIndex = 11;
+      this.btnEditDateCreated.UseVisualStyleBackColor = true;
+      // 
+      // btnEditCreator
+      // 
+      this.btnEditCreator.Location = new System.Drawing.Point(662, 85);
+      this.btnEditCreator.Name = "btnEditCreator";
+      this.btnEditCreator.Size = new System.Drawing.Size(88, 32);
+      this.btnEditCreator.TabIndex = 12;
+      this.btnEditCreator.UseVisualStyleBackColor = true;
+      // 
+      // btnChooseIndexing
+      // 
+      this.btnChooseIndexing.Location = new System.Drawing.Point(662, 123);
+      this.btnChooseIndexing.Name = "btnChooseIndexing";
+      this.btnChooseIndexing.Size = new System.Drawing.Size(88, 32);
+      this.btnChooseIndexing.TabIndex = 13;
+      this.btnChooseIndexing.UseVisualStyleBackColor = true;
+      // 
+      // lblDescriptionText
+      // 
+      this.lblDescriptionText.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+      this.lblDescriptionText.Location = new System.Drawing.Point(448, 203);
+      this.lblDescriptionText.Name = "lblDescriptionText";
+      this.lblDescriptionText.Size = new System.Drawing.Size(302, 192);
+      this.lblDescriptionText.TabIndex = 14;
+      this.lblDescriptionText.Text = "\r\n";
+      // 
+      // btnRemoveIndexItem
+      // 
+      this.btnRemoveIndexItem.Location = new System.Drawing.Point(344, 402);
+      this.btnRemoveIndexItem.Name = "btnRemoveIndexItem";
+      this.btnRemoveIndexItem.Size = new System.Drawing.Size(98, 38);
+      this.btnRemoveIndexItem.TabIndex = 15;
+      this.btnRemoveIndexItem.UseVisualStyleBackColor = true;
+      // 
+      // lblDescriptionCaption
+      // 
+      this.lblDescriptionCaption.Location = new System.Drawing.Point(448, 171);
+      this.lblDescriptionCaption.Name = "lblDescriptionCaption";
+      this.lblDescriptionCaption.Size = new System.Drawing.Size(188, 32);
+      this.lblDescriptionCaption.TabIndex = 16;
+      this.lblDescriptionCaption.Text = "\r\n";
+      this.lblDescriptionCaption.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+      // 
+      // btnEditDescription
+      // 
+      this.btnEditDescription.Location = new System.Drawing.Point(662, 168);
+      this.btnEditDescription.Name = "btnEditDescription";
+      this.btnEditDescription.Size = new System.Drawing.Size(88, 32);
+      this.btnEditDescription.TabIndex = 17;
+      this.btnEditDescription.UseVisualStyleBackColor = true;
+      // 
+      // btnDeleteBox
+      // 
+      this.btnDeleteBox.Location = new System.Drawing.Point(448, 402);
+      this.btnDeleteBox.Name = "btnDeleteBox";
+      this.btnDeleteBox.Size = new System.Drawing.Size(85, 38);
+      this.btnDeleteBox.TabIndex = 18;
+      this.btnDeleteBox.UseVisualStyleBackColor = true;
+      // 
+      // DialogManageBoxes
+      // 
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+      this.ClientSize = new System.Drawing.Size(763, 450);
+      this.Controls.Add(this.btnDeleteBox);
+      this.Controls.Add(this.btnEditDescription);
+      this.Controls.Add(this.lblDescriptionCaption);
+      this.Controls.Add(this.btnRemoveIndexItem);
+      this.Controls.Add(this.lblDescriptionText);
+      this.Controls.Add(this.btnChooseIndexing);
+      this.Controls.Add(this.btnEditCreator);
+      this.Controls.Add(this.btnEditDateCreated);
+      this.Controls.Add(this.btnAddIndexItem);
+      this.Controls.Add(this.buttonOpenBox);
+      this.Controls.Add(this.btnSaveBoxInfo);
+      this.Controls.Add(this.lblListCaption);
+      this.Controls.Add(this.listBox2);
+      this.Controls.Add(this.lblBoxIndexing);
+      this.Controls.Add(this.lblCreator);
+      this.Controls.Add(this.btnEditBoxName);
+      this.Controls.Add(this.lblDateCreated);
+      this.Controls.Add(this.lblBoxName);
+      this.Controls.Add(this.listBox1);
+      this.Font = new System.Drawing.Font("FangSong", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.Margin = new System.Windows.Forms.Padding(4);
+      this.Name = "DialogManageBoxes";
+      this.Text = "（默认语言）";
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.ListBox listBox1;
+    private System.Windows.Forms.Label lblBoxName;
+    private System.Windows.Forms.Label lblDateCreated;
+    private System.Windows.Forms.Button btnEditBoxName;
+    private System.Windows.Forms.Label lblCreator;
+    private System.Windows.Forms.Label lblBoxIndexing;
+    private System.Windows.Forms.ListBox listBox2;
+    private System.Windows.Forms.Label lblListCaption;
+    private System.Windows.Forms.Button btnSaveBoxInfo;
+    private System.Windows.Forms.Button buttonOpenBox;
+    private System.Windows.Forms.Button btnAddIndexItem;
+    private System.Windows.Forms.Button btnEditDateCreated;
+    private System.Windows.Forms.Button btnEditCreator;
+    private System.Windows.Forms.Button btnChooseIndexing;
+    private System.Windows.Forms.Label lblDescriptionText;
+    private System.Windows.Forms.Button btnRemoveIndexItem;
+    private System.Windows.Forms.Label lblDescriptionCaption;
+    private System.Windows.Forms.Button btnEditDescription;
+    private System.Windows.Forms.Button btnDeleteBox;
+  }
+}
